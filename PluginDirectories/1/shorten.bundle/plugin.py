d96dc1dc1ea21dd880fda1a4e44a27431318ee4c
hawkfalcon <hawkfalcon1@gmail.com>
2014-11-12 15:37:02 -0800

    Add new plugin: Goo.gl URL shortening

---
+++ PluginDirectories/1/shorten.bundle/plugin.py
@@ -0,0 +1,24 @@
+import urllib2
+import json
+
+def results(parsed, original_query):
+	return {
+		"title": "Shorten '{0}' (press enter)".format(parsed['~url']),
+		"run_args": [parsed['~url']]
+	}
+
+def run(message):
+	import os
+	os.system('echo ' + shorten(message) + ' | pbcopy')
+
+def shorten(url):
+    post_url = 'https://www.googleapis.com/urlshortener/v1/url'
+    postdata = {'longUrl':url}
+    headers = {'Content-Type':'application/json'}
+    req = urllib2.Request(
+        post_url,
+        json.dumps(postdata),
+        headers
+    )
+    ret = urllib2.urlopen(req).read()
+    return json.loads(ret)['id']
