4bfe87b5b41da7909b1335ec1602a93acb0a3c55
nate-parrott <nateparro2t@gmail.com>
2014-11-12 13:06:20 -0500

    minor adjustments to imdb and yelp; regen index

--- PluginDirectories/1/imdb.bundle/plugin.py
+++ PluginDirectories/1/imdb.bundle/plugin.py
@@ -2,10 +2,11 @@ import urllib, json
 
 def results(parsed, original_query):
 	search_specs = [
-		 ["IMDB", "~imdbquery", "http://m.imdb.com/find?q="]
+		 ["IMDB", "~imdbquery", "http://m.imdb.com/find?q=", "http://imbdb.com/find?q="]
 	]
-	for name, key, url in search_specs:
+	for name, key, mobile_url, url in search_specs:
 		if key in parsed:
+			mobile_search_url = mobile_url + urllib.quote_plus(parsed[key])
 			search_url = url + urllib.quote_plus(parsed[key])
 			return {
 				"title": "Search {0} for '{1}'".format(name, parsed[key]),
@@ -14,9 +15,9 @@ def results(parsed, original_query):
 				<script>
 				setTimeout(function() {
 					window.location = %s
-				}, 100);
+				}, 500);
 				</script>
-				"""%(json.dumps(search_url)),
+				"""%(json.dumps(mobile_search_url)),
 				"webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
 				"webview_links_open_in_browser": True
 			}
