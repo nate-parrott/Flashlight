9da396df22a74730a81d609efe5f9e9d8825ae19
nate-parrott <nateparro2t@gmail.com>
2014-11-14 14:21:07 -0500

    Improve translation plugin

--- PluginDirectories/1/Instranslator.bundle/plugin.py
+++ PluginDirectories/1/Instranslator.bundle/plugin.py
@@ -1,19 +1,29 @@
-lang = {
-	'spanish': 'es',
-	'english': 'en',
-	'arab': 'ar',
-	"french" : "fr",
-	"german" : "de",
-	'hindi' : "hi",
-	'portuguese' : "pt",
+lang_codes = {
+    'spanish': 'es',
+    'english': 'en',
+    'arabic': 'ar',
+    "french" : "fr",
+    "german" : "de",
+    'hindi' : "hi",
+    'chinese' : "zh",
+    "auto" : "auto"
 }
 
 def results(parsed, original_query):
     text = parsed['~text']
-    query_language = original_query.split(" ")[-1].lower()
-    lan = lang[query_language]
-    html = open("translate.html").read().replace("----words----", text).replace("----lang----", lan)
+    from_lang = "auto"
+    to_lang = "spanish"
+    for key, val in parsed.iteritems():
+        if key.startswith('from_language/'):
+            from_lang = key.split('/')[1]
+        elif key.startswith('to_language/'):
+            to_lang = key.split('/')[1]
+    html = open("translate.html").read().replace("----text----", text).replace("----fromlang----", lang_codes[from_lang]).replace("----tolang----", lang_codes[to_lang])
+    if from_lang == 'auto':
+        title = "Translate \"{0}\" to {1}".format(text, to_lang)
+    else:
+        title = "Translate \"{0}\" from {1} to {2}".format(text, from_lang, to_lang)
     return {
-        "title": "Showing {0} in {1}".format(text, query_language),
-        "html": html,
+        "title": title,
+        "html": html
     }
