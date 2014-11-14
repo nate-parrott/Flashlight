ed305e1108b7ca2f4b3be08764f96bb882120fd1
Andy Venet <andy.venet@gmail.com>
2014-11-14 11:45:58 -0300

    Replacing tabs by four spaces in all .py files closes nate-parrott/Flashlight#35

--- PluginDirectories/1/Instranslator.bundle/plugin.py
+++ PluginDirectories/1/Instranslator.bundle/plugin.py
@@ -1,7 +1,7 @@
 def results(parsed, original_query):
-	text = parsed['~text']
-	html = open("translate.html").read().replace("----words----", text)
-	return {
-		"title": "Translate {0} to Spanish".format(parsed['~text']),
-		"html": html,
-	}
+    text = parsed['~text']
+    html = open("translate.html").read().replace("----words----", text)
+    return {
+        "title": "Translate {0} to Spanish".format(parsed['~text']),
+        "html": html,
+    }
