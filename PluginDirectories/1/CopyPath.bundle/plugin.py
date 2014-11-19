7da89e8e0cb66b49f875b018301f1f2b3873b2c7
xuxiaoteng <xuxiaoteng@bravesoft.co.jp>
2014-11-19 17:23:21 +0900

    Plugin: copy path to clipboard from finder

---
+++ PluginDirectories/1/CopyPath.bundle/plugin.py
@@ -0,0 +1,20 @@
+import os
+
+def results(parsed, original_query):
+    return {
+        "title": "Copy Path",
+            "run_args": ['']
+    }
+
+def run(command):
+	from applescript import asrun, asquote
+	# TODO: set the current working dir to the frontmost dir in Finder
+	ascript = '''
+	tell application "Finder"
+		set theItems to selection
+		set filePath to (POSIX path of (the selection as alias))
+	end tell
+	set the clipboard to filePath
+	'''.format(asquote(command))
+
+	asrun(ascript)
