230b81e249fff24d98b42d0ae12c45f47655e344
matth96 <Matthias@MacBook-Pro.local>
2014-11-26 09:51:19 +0100

    add dutch translation

---
+++ PluginDirectories/1/wikipedia.bundle/plugin_nl.py
@@ -0,0 +1,27 @@
+import urllib, json
+
+def results(parsed, original_query):
+    search_specs = [
+          ["Nederlandse Wikipedia", "~enWiki", "https://nl.m.wikipedia.org/w/index.php?search=", "https://nl.wikipedia.org/w/index.php?search="],
+    ]
+    for name, key, mobile_url, url in search_specs:
+        if key in parsed:
+            mobile_search_url = mobile_url + urllib.quote_plus(parsed[key])
+            search_url = url + urllib.quote_plus(parsed[key])
+            return {
+                "title": "Search {0} for '{1}'".format(name, parsed[key]),
+                "run_args": [search_url],
+                "html": """
+                <script>
+                setTimeout(function() {
+                    window.location = %s
+                }, 500);
+                </script>
+                """%(json.dumps(mobile_search_url)),
+                "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
+                "webview_links_open_in_browser": True
+            }
+
+def run(url):
+    import os
+    os.system('open "{0}"'.format(url))
