94a45e8cae04f2e8abb11e4c1ebaf46fbc5c3564
Jia Sui <jsfaint@gmail.com>
2014-11-30 17:56:07 +0800

    Modify devdocs.io match condition to avoid mismatch.

--- PluginDirectories/1/devdocs.bundle/plugin.py
+++ PluginDirectories/1/devdocs.bundle/plugin.py
@@ -2,14 +2,16 @@ import os
 import urllib
 
 def results(parsed, original_query):
-    key=urllib.quote(parsed["~query"])
-    url="http://devdocs.io/#q=" + key
+    key="~query"
 
-    return {
-        "title": "Search '{0}' on devdocs.io".format(key),
-        "run_args": [url],
-        "webview_links_open_in_browser": True
-    }
+    if key in parsed:
+        url="http://devdocs.io/#q=" + urllib.quote(parsed[key])
+        return {
+                "title": "Search '{0}' on devdocs.io".format(parsed[key]),
+                "run_args": [url],
+                "webview_links_open_in_browser": True
+                }
+
 
 def run(str):
     os.system('open "{0}"'.format(str))
