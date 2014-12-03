2aaebd9e9294954f93ce93e181db5bd5b473b9fc
Toni Hoffmann <toni.hoffmann@arthrex.de>
2014-12-03 12:46:06 +0100

    further work on gas price feature

--- PluginDirectories/1/gasprice.bundle/plugin.py
+++ PluginDirectories/1/gasprice.bundle/plugin.py
@@ -21,7 +21,5 @@ def results(parsed, original_query):
 	return {
 
         "title": title,
-        "html": open(i18n.find_localized_path("mehrtanken.html")).read().replace("SEARCHQUERY", parsed[query]).replace("FUELSETTING",benzin).replace("light-mode", "dark-mode" if dark_mode() else "light-mode"),
-        "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
-        "webview_links_open_in_browser": True
+        "html": open(i18n.find_localized_path("mehrtanken.html")).read().replace("SEARCHQUERY", parsed[query]).replace("FUELSETTING",benzin).replace("light-mode", "dark-mode" if dark_mode() else "light-mode")
     }
