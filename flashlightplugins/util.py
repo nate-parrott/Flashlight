f277c4dd8a85fb03f18ab6be59681941ae4aa8cf
nate-parrott <nateparro2t@gmail.com>
2014-12-03 02:18:06 -0500

    randomly sort categories each day

--- flashlightplugins/util.py
+++ flashlightplugins/util.py
@@ -1,4 +1,4 @@
-import jinja2, os
+import jinja2, os, calendar, time, random
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -8,3 +8,13 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 def template(name, vars={}):
   template = JINJA_ENVIRONMENT.get_template(name)
   return template.render(vars)
+
+def stable_daily_shuffle(items):
+  timestamp = int(calendar.timegm(time.gmtime()))
+  day = int(timestamp / (24 * 60 * 60))
+  r = random.Random()
+  r.seed(day)
+  items = items[:]
+  r.shuffle(items)
+  return items
+  
