8226e4c11cea652a38b75c92f69e1965c11435e0
xremix <tonidhoffmann@gmail.com>
2014-12-10 15:23:06 +0100

    Added Show in Finder Plugin

---
+++ PluginDirectories/1/showinfinder.bundle/plugin.py
@@ -0,0 +1,35 @@
+import urllib, json, i18n, os
+
+def dark_mode():
+    import Foundation
+    return Foundation.NSUserDefaults.standardUserDefaults().persistentDomainForName_(Foundation.NSGlobalDomain).objectForKey_("AppleInterfaceStyle") == "Dark"
+
+def getFiles(url):
+    import glob
+    url = url if(url != "/") else ""
+    return glob.glob(url+'/*')
+
+def generateHtml(list):
+    color = "#E6E6E6" if dark_mode() else "#2D2D2D"
+    html = '<div style="color:'+color+';">'
+    html += '<br/>'.join(list)
+    html += '</div>'
+    return html
+
+def results(parsed, original_query):
+    url = parsed["~path"]
+    os.chdir(os.path.expanduser("~"))
+    if(os.path.exists(url) == False):
+        url = os.path.expanduser("~")
+    else:
+        url = os.path.abspath(url)
+    return {
+        "title": i18n.localstr("Show '{0}' in Finder").format(url),
+        "run_args": [url],
+        "webview_transparent_background": True,
+        "html": generateHtml(getFiles(url))
+    }
+
+def run(url):
+    import subprocess
+    subprocess.call(["open", "-R", url])
