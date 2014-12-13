cbfd69c1f172750cb4c494523271306f0faedfa8
nate-parrott <nateparro2t@gmail.com>
2014-12-13 01:39:30 -0500

    fix overzealous plugin matching

--- NaturalCommands/parse_query.py
+++ NaturalCommands/parse_query.py
@@ -21,6 +21,9 @@ def create_example_phrases():
     example_phrases.append(commanding.Phrase("", ["eyfght oehrgueig erobf", ["ehheiog","hegoegn"]]))
     example_phrases.append(commanding.Phrase("", ["wurt turt gurt", ["~burt", "nurt"]]))
     example_phrases.append(commanding.Phrase("", [["~uirguieg", "hgeough egoiheroi"]]))
+    example_phrases.append(commanding.Phrase("", [["~uirguieg", "hgeough egoiheroi"]]))
+    example_phrases.append(commanding.Phrase("", [["~uirguieg", "hgeough egoiheroi ehgiegeg"]]))
+    example_phrases.append(commanding.Phrase("", [["~uirguieg", "hgeough egoiheroi ehgiegeg riehg hierohgi"]]))
     example_phrases.append(commanding.Phrase("", [["~uirguieg", "hgeoughegoiheroi"]]))
 
     for plugin in os.listdir(plugin_dir):
