7f1b9c3d7ce5c5bb9eb0a0be9fddde4ec5ebbbd3
xremix <tonidhoffmann@gmail.com>
2014-12-17 15:51:35 +0100

    Fixed gif plugin, added random image plugin

---
+++ PluginDirectories/1/randomimage.bundle/plugin.py
@@ -0,0 +1,18 @@
+import i18n
+
+def dark_mode():
+    import Foundation
+    return Foundation.NSUserDefaults.standardUserDefaults().persistentDomainForName_(Foundation.NSGlobalDomain).objectForKey_("AppleInterfaceStyle") == "Dark"
+
+def results(parsed, original):
+  import json
+  path = "imagebot.html";
+  html = open(i18n.find_localized_path(path)).read().replace("var nullValue = null;", "var nullValue = \"{0}\";").replace('{0}', parsed['~query']).replace("light-mode", "dark-mode" if dark_mode() else "light-mode")
+  return {
+    "title": i18n.localstr(u"\"{0}\" Random Image (Press cmd + c to copy link)").format(parsed['~query']),
+    "html": html,
+    "pass_result_of_output_function_as_first_run_arg": True,
+    "run_args": ["abc"],
+    "webview_transparent_background": True,
+    "webview_links_open_in_browser": True
+  }
