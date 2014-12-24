c9ec1742c7348978057e2cd6e4fd385e323bd359
nate-parrott <nateparro2t@gmail.com>
2014-12-24 11:23:59 -0800

    refactor + fix directory bug

--- flashlightplugins/main.py
+++ flashlightplugins/main.py
@@ -34,7 +34,8 @@ from google.appengine.api import users
 import bs4
 import model
 from model import Plugin
-from search import search_plugins
+from directory import directory_html
+from util import get_localized_key
 
 
 def send_upload_form(request, message=None):
@@ -79,24 +80,6 @@ def read_plugin_info(plugin, zip_data):
             screenshot = archive.open(name).read()
             plugin.screenshot_url = resize_and_store(screenshot, 800)
     return has_info
-
-def language_suffixes(languages):
-    for lang in languages:
-        while True:
-            yield "_" + lang if lang != 'en' else ''
-            if '-' in lang:
-                lang = lang[:lang.rfind('-')]
-            else:
-                break
-    yield ''
-
-
-def get_localized_key(dict, name, languages, default=None):
-    for suffix in language_suffixes(languages):
-        key = name + suffix
-        if key in dict:
-            return dict[key]
-    return default
 
 
 class PostUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
@@ -152,66 +135,6 @@ class PostUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
             message = "Your plugin was uploaded!" + approval_msg
             self.response.write(template("uploaded.html", {"message": message,
                                                            "plugin": plugin}))
-
-
-def arrays_overlap(a1, a2):
-  return sum([1 for a in a1 if a in a2]) > 0
-
-def locales_overlap(l1, l2):
-  normalize_locale = lambda locale: locale.split('-')[0]
-  return arrays_overlap(map(normalize_locale, l1), map(normalize_locale, l2))
-
-def group_plugins(plugin_dicts, languages, languages_were_specified):
-  if languages_were_specified:
-    plugins_for_other_locales = [p for p in plugin_dicts if ('preferred_locales' in p and not locales_overlap(p['preferred_locales'], languages))]
-    plugins_for_other_locales_names = set([p['name'] for p in plugins_for_other_locales])
-    native_plugins = [p for p in plugin_dicts if p['name'] not in plugins_for_other_locales_names]
-    groups = [
-      {"plugins": native_plugins},
-      {"plugins": plugins_for_other_locales, "header": "Plugins for other regions", "class": "other_locales"}
-    ]
-  else:
-    groups = [{"plugins": plugin_dicts}]
-  return [g for g in groups if len(g['plugins'])]
-
-def directory_html(category=None, search=None, languages=None, browse=False,
-                   name=None):
-    languages_specified = languages != None
-    if not languages_specified:
-      languages = ['en']
-    if category:
-        plugins = list(Plugin.query(Plugin.categories == category,
-                                    Plugin.approved == True))
-        plugins = stable_daily_shuffle(plugins)
-    elif search:
-        plugins = search_plugins(search)
-    elif name:
-        plugin = Plugin.by_name(name)
-        plugins = [plugin] if plugin else []
-    else:
-        plugins = []
-    plugin_dicts = []
-    for p in plugins:
-        plugin = info_dict_for_plugin(p, languages)
-        plugin_dicts.append(plugin)
-    groups = group_plugins(plugin_dicts, languages, languages_specified)
-    return template("directory.html",
-                    {"groups": groups, "browse": browse,
-                     "search": search})
-
-
-def info_dict_for_plugin(p, languages=['en']):
-    plugin = json.loads(p.info_json)
-    plugin['displayName'] = get_localized_key(plugin, "displayName", languages,
-                                              "")
-    plugin['description'] = get_localized_key(plugin, "description", languages,
-                                              "")
-    plugin['examples'] = get_localized_key(plugin, "examples", languages, [])
-    plugin['model'] = p
-    plugin['install_url'] = 'install://_?' + \
-                            urllib.urlencode([("zip_url", p.zip_url),
-                                              ("name", p.name.encode('utf8'))])
-    return plugin
 
 
 class Directory(webapp2.RequestHandler):
