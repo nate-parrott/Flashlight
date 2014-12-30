76285fe417c48f36e46866f0f4ecaf16b4d73636
nate-parrott <nateparro2t@gmail.com>
2014-12-29 21:09:35 -0500

    minor changes to lorem ipsum

--- PluginDirectories/1/lipsum.bundle/plugin.py
+++ PluginDirectories/1/lipsum.bundle/plugin.py
@@ -11,10 +11,28 @@ def results(fields, original_query):
     what = hw[1]
     lipsum = pypsum.get_lipsum(how, what, "no")
     output = lipsum[0].replace('\n', '<br /><br />')
+    
+    html = """
+    <style>
+    body {
+      font-family: "HelveticaNeue";
+      line-height: 1.3;
+      padding: 10px;
+    }
+    #info {
+      font-style: italic;
+      font-size: small;
+      opacity: 0.7;
+    }
+    </style>
+    <p><!--OUTPUT--></p>
+    <p id='info'><!--INFO-->. Press enter to copy it.</p> 
+    """.replace("<!--OUTPUT-->", output).replace("<!--INFO-->", lipsum[1])
+    
     return {
         "title": "Lorem Ipsum %s %s" % (how, what),
         "run_args": [output] ,
-        "html": output + "<br /><br /><i>" + lipsum[1] + "</i>",
+        "html": html,
         "webview_transparent_background": True,
     }
 
