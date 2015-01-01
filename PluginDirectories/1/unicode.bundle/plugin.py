180c5406a502f652af42f84095a76af1babe0aa7
Thomas Eschemann <tom@eschemann.fr>
2015-01-01 15:23:52 +0100

    improve search function in unicode plugin

--- PluginDirectories/1/unicode.bundle/plugin.py
+++ PluginDirectories/1/unicode.bundle/plugin.py
@@ -3,9 +3,14 @@
 import i18n, os
 
 def results(fields, original_query):
-    query = fields['~query']
-    title = i18n.localstr('Search Unicode characters for \'{0}\'').format(query)
-    html = open(i18n.find_localized_path('unicode.html')).read().decode('utf-8').replace("%query%", query)
+    html = open(i18n.find_localized_path('unicode.html')).read().decode('utf-8')
+    if("~emoji" in fields):
+        query = fields['~emoji']
+        html = html.replace("%query%", query).replace("%type%", "emoji")
+    else:
+        query = fields['~query']
+        html = html.replace("%query%", query).replace("%type%", "characters")
+    title = i18n.localstr('Search emoji for \'{0}\'').format(query)
     return {
         "title": title,
         "html": html,
