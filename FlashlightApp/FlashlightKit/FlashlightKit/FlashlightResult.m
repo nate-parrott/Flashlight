b5c969577b96da409f9e666349bf7d372f70152f
nate-parrott <nateparro2t@gmail.com>
2015-01-07 02:28:24 -0500

    fixed plugin-running bug

--- FlashlightApp/FlashlightKit/FlashlightKit/FlashlightResult.m
+++ FlashlightApp/FlashlightKit/FlashlightKit/FlashlightResult.m
@@ -57,7 +57,7 @@
                                 @"pluginPath": self.pluginPath
                                 };
         task.arguments = @[input.toJson];
-        [task launchWithTimeout:2 callback:^(NSData *stdoutData, NSData *stderrData) {
+        [task launchWithTimeout:20 callback:^(NSData *stdoutData, NSData *stderrData) {
             NSString *error = nil;
             if (stderrData) {
                 error = [[NSString alloc] initWithData:stderrData encoding:NSUTF8StringEncoding];
