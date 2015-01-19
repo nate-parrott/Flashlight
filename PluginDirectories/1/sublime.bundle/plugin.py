d381bb65a0d3351e23db7165c48cb53c7f0d9456
Marc Bachmann <marc.brookman@gmail.com>
2015-01-19 23:59:20 +0100

    Update sublime plugin with find & styles

--- PluginDirectories/1/sublime.bundle/plugin.py
+++ PluginDirectories/1/sublime.bundle/plugin.py
@@ -1,5 +1,5 @@
 import json, subprocess
-
+import os
 
 def appearance():
     import Foundation
@@ -12,49 +12,138 @@ def build_html(file, directories):
     <html>
     <head>
         <style>
-            body{
-                padding: 10px 12px;
+            * {
+                box-sizing: border-box;
+            }
+
+            html, body{
+                position: absolute;
+                left: 0;
+                right: 0;
+                top: 0;
+                bottom: 0;
+                margin: 0;
+                padding: 0;
                 font: 15px/1.4 'Helvetica Neue';
                 font-weight: 300;
                 -webkit-user-select: none;
-            }
-
-            h1 {
-                font-size: 20px;
-                font-weight: 300;
-            }
-
-            h1 small {
-                margin-left: 5px;
-                color: rgb(119,119,119);
-            }
-
-            label, small {
-                font-size: 12px;
                 overflow: hidden;
-                white-space: nowrap;
             }
 
-            label {
+            .featured {
+                position: absolute;
+                top: 27px;
+                left: 22px;
+                right: 22px;
+                height: 70px;
+                border-bottom: 1px solid rgb(217,217,217);
+                overflow: hidden;
+            }
+
+            .featured .icon {
                 display: block;
-                font-size: 11px;
-                -webkit-user-select: all;
+                float: left;
+                width: 63px;
+                height: 50px;
+                background: transparent url('folder.png') no-repeat;
+                background-size: cover;
+                margin-right: 22px;
             }
 
-            .highlight {
+            .featured .name {
+                color: black;
+                font-size: 20px;
+                font-weiht: 400;
+            }
+
+            .content {
+                position: absolute;
+                top: 0;
+                right: 0;
+                left: 0;
+                width: 100vw;
+                bottom: 0;
+                overflow-y: auto;
+            }
+
+            body.withFeatured .content {
+                top: 96px;
+            }
+
+            ul {
+                margin: 0;
+                padding: 0;
+                list-style: none;
             }
 
             ul li {
-                border-bototm
+                position: relative;
+                height: 45px;
+                padding: 3px 22px;
+            }
+
+            ul li .separator {
+                position: absolute;
+                left: 22px; right: 22px;
+                padding: 3px 0 0;
+                border-bottom: 1px solid rgb(217,217,217);
+                bottom: -1px;
+            }
+
+            ul li .icon {
+                position: relative;
+                top: 7px;
+                float: left;
+                width: 32px;
+                height: 25px;
+                margin-right: 20px;
+                background: transparent url('folder.png') no-repeat;
+                background-size: cover;
+            }
+
+            ul li:active, ul li:active .description {
+                background: rgb(0,123, 255);
+                color: white;
+            }
+
+            .name {
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                overflow: hidden;
+            }
+
+            .description {
+                font-size: 12px;
+                color: rgb(145, 145, 145);
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
             }
 
             .dark {
                 color: rgb(224,224,224);
             }
+
+            .dark .featured {
+                border-bottom: 1px solid rgb(119, 119, 119);
+            }
+
+            .dark .separator {
+                border-bottom: 1px solid rgb(119,119,119);
+            }
+
+            .dark .name {
+                color: rgb(224,224,224);
+            }
         </style>
+        <script>
+            function openPath(path) {
+                command = 'open -a "Sublime Text" {{path}}';
+                flashlight.bash(command.replace(/{{path}}/g, path));
+            }
+        </script>
     </head>
-    <body class="{{appearance}}">
-        <div class="message"></div>
+    <body class="{{appearance}}{{has_feature}}">
         {{content}}
     </body>
     </html>
@@ -62,24 +151,29 @@ def build_html(file, directories):
 
     content = ''
     if file:
+        html = html.replace('{{has_feature}}', ' withFeatured')
         content += """
-        <div class="highlight">
+        <div class="featured">
             <span class="icon"></span>
             <div class="name">%s</div>
-            <div class="items">9 items</div>
+            <div class="description">%s</div>
         </div>
-        """ % (file)
+        """ % (os.path.basename(file), os.path.dirname(file))
+    else:
+        html = html.replace('{{has_feature}}', '')
 
-    content += '<ul>'
+
+    content += '<div class="content"><ul>'
     for directory in directories:
         content += """
-        <li>
+        <li onclick="openPath('%s')">
             <span class="icon"></span>
             <div class="name">%s</div>
-            <div class="items">9 items</div>
+            <div class="description">%s</div>
+            <div class="separator"></div>
         </li>
-        """ % (directory)
-    content += '</ul>'
+        """ % (directory, os.path.basename(directory), os.path.dirname(directory)+'/')
+    content += '</ul></div>'
 
     return html.replace("{{content}}", content)
 
@@ -92,15 +186,33 @@ def get_alias(name, aliases):
 
 def results(params, original_query):
     query = params['~query'] if params.has_key('~query') else ''
+    if len(query) < 3: query = ''
     config = json.load(open('preferences.json'))
-    title = "Open with Sublime Text"
-    files = subprocess.check_output("mdfind -name '{{query}}' | grep -v '/Library'".replace('{{query}}', query), shell=True).split('\n')[:20]
-
+    title = 'Open current selection with Sublime Text'
     file = get_alias(query, config['aliases'])
-    file_to_open = file or files[0]
+    files = []
+
+    if query:
+        files = subprocess.check_output("mdfind 'kind:folder {{query}}'".replace('{{query}}', query), shell=True).strip()
+        files = files.split('\n')[:30]
+        files = filter(lambda file: len(file) and '/Library' not in file, files)
+        user_home = os.getenv("HOME")
+        files = map(lambda file: file.replace(user_home, '~'), files)
+
+    if not file and len(files):
+        file_to_open = files[0]
+    else:
+        file_to_open = file
+
+    if not len(files):
+        for val in config['aliases'] or ():
+            if val.has_key('path'): files.append(val['path'])
+
+    if file_to_open:
+        title = 'Open '+ file_to_open +' in Sublime Text'
 
     return {
-        "title": query,
+        "title": title,
         'html': build_html(file, files),
         "run_args": [file_to_open],
         'webview_transparent_background': True
@@ -145,6 +257,6 @@ def open_finder_item():
     applescript.asrun(ascript)
 
 
-# print results({'~query': 'home'}, 'sublime livingdocs-api')
+# print results({'~query': 'foobartest'}, 'sublime livingdocs-api')['run_args']
 # print run(results({'~query': 'home'}, 'sublime livingdocs-api')['run_args'][0])
 # mdfind 'livingdocs-api' | grep -v '/Library'
