bb9e28e763f05c5ef671d2a32a79784ed4aa865f
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-01-22 14:08:35 +0900

    Update ninetan plugin

--- PluginDirectories/1/ninetan.bundle/plugin.py
+++ PluginDirectories/1/ninetan.bundle/plugin.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
+import i18n
+
+
 locations = {
     'chiba': 'chiba',
     'hyogo': 'hyogo',
@@ -18,10 +23,14 @@ locations = {
     '六本木': 'roppongi',
     '静岡': 'shizuoka',
     '東京': 'tokyo',
-    '和歌山': 'wakayama',
+    '和歌山': 'wakayama'
 }
 
+
 def results(parsed, original_query):
+    if '~location' not in parsed:
+        return
+
     location = parsed['~location'].lower()
     keys = list(filter(lambda l: l.startswith(location), locations.keys()))
 
@@ -36,16 +45,18 @@ def results(parsed, original_query):
         </script>
         """
         return {
-            "title": "Ninetan '{0}'".format(location),
+            "title": i18n.localstr("Ninetan @{0}").format(
+                i18n.localstr(location.capitalize())),
             "run_args": [url],
             "html": html,
             "webview_links_open_in_browser": True
         }
     else:
         return {
-            "title": "Ninetan location not found",
+            "title": i18n.localstr("Ninetan location not found"),
             "run_args": [None]
         }
+
 
 def run(url):
     if url is not None:
