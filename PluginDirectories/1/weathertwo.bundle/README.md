15baac36bf9e1da3932f4d63e041ce2b5c4ff398
Sebastian Grans <sebastian.grans@gmail.com>
2015-02-20 17:14:02 +0100

    An updated weather plugin.
    I implemented an hour-by-hour view in the weather plugin. It is
    activated by the keyword “now”, eg. by searching for “weather place
    now”.

---
+++ PluginDirectories/1/weathertwo.bundle/README.md
@@ -0,0 +1,4 @@
+### Ideas
+- OpenWeatherMap has multilingual support which could be implemented.
+- Present more information such as highs and lows, wind-speed, etc.
+- If you choose to open the site, you should directly go to [http://openweathermap.org/city/\{city code\}](http://openweathermap.org/city/2666199) instead of the search results page.
---
+++ PluginDirectories/1/weathertwo.bundle/plugin.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import sys, urllib, os
+import AppKit
+import i18n
+
+
+# Are you using the "dark" style in OS X?
+# System Preferences > General > Use dark menu bar and Dock.
+def dark_mode():
+    import Foundation
+    return Foundation.NSUserDefaults.standardUserDefaults().persistentDomainForName_(Foundation.NSGlobalDomain).objectForKey_("AppleInterfaceStyle") == "Dark"
+
+
+# Is the computer locale metric or imperial?
+def use_metric():
+    return AppKit.NSLocale.currentLocale().objectForKey_(AppKit.NSLocaleUsesMetricSystem)
+
+
+def results(parsed, original_query):
+
+    location = parsed['~location']
+    if '~now' in parsed:
+        if parsed['~now'] == 'now':
+            time = 'now'
+        else:
+            time = 'later'
+    else:
+        time = 'later'
+
+    # Used for debugging
+    # print >> sys.stderr, 'Original query: ' + original_query
+    # print >> sys.stderr, 'Interpreted the time as: ' + time
+    # print >> sys.stderr, 'Interpreted the location as: ' + location
+
+    title = i18n.localstr('"{0}" weather').format(location)
+    html = (
+        open(i18n.find_localized_path("weather.html")).read().decode('utf-8')
+        .replace("<!--LOCATION-->", location)
+        .replace("<!--UNITS-->", "metric" if use_metric() else "imperial")
+        .replace("<!--APPEARANCE-->", "dark" if dark_mode() else "light")
+        .replace("<!--TIME-->", time)
+        # OpenWeatherMap supports a range of different languages.
+        #.replace("<!--locale-->") # Future implementation? 
+    )
+
+    return {
+        "title": title,
+        "html": html,
+        "webview_transparent_background": True,
+        "run_args": [location]
+    }
+
+
+def run(location):
+    os.system('open "http://openweathermap.org/find?q={0}"'.format(urllib.quote(location)))
