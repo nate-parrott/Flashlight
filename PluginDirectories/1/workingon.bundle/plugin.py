f6656ee674e7836bafd66dcbc5aca26e2347c9a7
nate-parrott <nateparro2t@gmail.com>
2015-03-04 14:39:57 -0500

    update workingon

--- PluginDirectories/1/workingon.bundle/plugin.py
+++ PluginDirectories/1/workingon.bundle/plugin.py
@@ -2,14 +2,17 @@ import json
 import pipes
 
 def results(fields, original_query):
-  message = fields['~message']
-  return {
-    "title": "on {0}".format(message),
-    "run_args": [message],
+	message = fields['~message']
+	return {
+		"title": "on {0}".format(message),
+		"run_args": [message],
 	"html": "<h1 style='font-family: sans-serif; padding: 2em'>I'm working on {0}</h1>".format(message)
-  }
+	}
 
 def run(message):
-  import os
-  token = json.load(open('preferences.json'))['token']
-  os.system('curl -X POST --data-urlencode "task={0}" https://api.workingon.co/hooks/incoming?token={1} >/dev/null 2>&1;'.format(pipes.quote(message.encode('utf8')),token))
+	import os
+	token = json.load(open('preferences.json'))['token']
+	if len(token) == 0:
+		os.system('open "flashlight://plugin/workingon/preferences"')
+	else:
+		os.system('curl -X POST --data-urlencode "task={0}" https://api.workingon.co/hooks/incoming?token={1} >/dev/null 2>&1;'.format(pipes.quote(message.encode('utf8')),token))
