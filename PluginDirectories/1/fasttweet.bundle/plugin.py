3e9c9c0b7e15f1d50040369e105e714b61f57342
Андрей <anechytailov@gmail.com>
2015-03-21 11:27:38 +0200

    Fixed missed renaming

--- PluginDirectories/1/fasttweet.bundle/plugin.py
+++ PluginDirectories/1/fasttweet.bundle/plugin.py
@@ -23,11 +23,10 @@ def results(parsed, original_query):
                     window.location = %s
                 }, 500);
                 </script>
-                """ % (json.dumps(search_url)),
+                """ % (json.dumps(tweet_url)),
                 "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
                 "webview_links_open_in_browser": True
             }
-
 
 def run(url):
     import os
