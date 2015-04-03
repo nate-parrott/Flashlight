4bee29dad5a7ed307f6f2681c71590bebba22137
nate-parrott <nateparro2t@gmail.com>
2015-04-03 16:21:29 -0400

    update

--- PluginDirectories/1/imessage.bundle/plugin.py
+++ PluginDirectories/1/imessage.bundle/plugin.py
@@ -1,25 +1,63 @@
+import re
 
 def results(parsed, original_query, obj):
-    dict = {
-        "title": "Send an iMessage",
-        "run_args": [obj.multitags()],
-        "html": html(obj.multitags()),
-        "webview_transparent_background": True
-    }
-    return dict
+		dict = {
+				"title": "Send an iMessage",
+				"run_args": [parsed],
+				"html": html(parsed),
+				"webview_transparent_background": True
+		}
+		return dict
 
+def get_contact_from_recip(recip):
+	if len(recip.keys()) > 0 and recip.keys()[0].startswith('@contact/'):
+		return recip.values()[0]
+	else:
+		return None
+
+def get_recipient_id(parsed):
+	recip = parsed.get('recip', {})
+	contact = get_contact_from_recip(recip)
+	if contact:
+		preferred_phone_labels = ["iPhone", "_$!<Mobile>!$_", "_$!<Main>!$_"]
+		for label in preferred_phone_labels:
+			if label in contact.get('Phone', {}):
+				return normalize_phone(contact['Phone'][label])
+		for label, email in contact.get('Email', {}).iteritems():
+			return email
+	if 'email' in recip:
+		return recip['email']
+	if 'phone' in recip:
+		return normalize_phone(recip['phone'])
+	return None
+
+def normalize_phone(num):
+	drop = ' -.()'
+	for c in drop:
+		num = num.replace(c, '')
+	if len(num) > 5 and re.match(r"^[0-9]+$", num):
+		return num
+	else:
+		return None
+
+def get_recipient_name(parsed):
+	recip = parsed.get('recip', {})
+	contact = get_contact_from_recip(recip)
+	if contact and 'displayName' in contact:
+		return contact['displayName']
+	return get_recipient_id(parsed)
 
 def html(parsed):
-    recips = parsed.get('~_PersonalName', [])
-    body = parsed.get('~message', [''])[0]
-    attach = "<img src='paper-clip.png'/> Any files currently selected in Finder will be attached." if 'include_files' in parsed else ""
-    return open('html.html').read().replace("<!--RECIPIENTS-->", ", ".join(recips)).replace("<!--BODY-->", body).replace("<!--ATTACH-->", attach)
+		recipients = [get_recipient_name(parsed)] if get_recipient_name(parsed) else []
+		body = parsed.get('~message', '')
+		# TODO: attachments
+		attach = ""
+		return open('html.html').read().replace("<!--RECIPIENTS-->", ", ".join(recipients)).replace("<!--BODY-->", body).replace("<!--ATTACH-->", attach)
 
 def run(parsed):
-    recips = parsed.get('~_PersonalName', [])
-    if len(recips) != 1: return
-    recip = recips[0]
-    body = parsed.get('~message', [''])[0]
-    attach = 'include_files' in parsed
-    from send_message import send_message
-    send_message(recip, body, attach)
+		recip = get_recipient_id(parsed)
+		if recip:
+			body = parsed.get('~message', [''])
+			attach = False # TODO
+			from send_message import send_message
+			send_message(recip, body, attach)
--- PluginDirectories/1/imessage.bundle/send_message.py
+++ PluginDirectories/1/imessage.bundle/send_message.py
@@ -4,50 +4,40 @@ from applescript import asrun, asquote
 import re
 
 def normalize_phone(num):
-  drop = ' -.'
-  for c in drop:
-    num = num.replace(c, '')
-  if len(num) > 5 and re.match(r"^[0-9]+$", num):
-    return num
-  else:
-    return None
+	drop = ' -.'
+	for c in drop:
+		num = num.replace(c, '')
+	if len(num) > 5 and re.match(r"^[0-9]+$", num):
+		return num
+	else:
+		return None
 
 def send_message(recipient, body, attach_selected_files):
-    buddy = None
-    if normalize_phone(recipient):
-      buddy = normalize_phone(recipient)
-    else:
-      address_book = contacts.address_book_to_list()
-      result = contacts.find_contact(recipient, address_book, "phone")
-      if result:
-        buddy = result['phone'][0]
-    if not buddy:
-      asrun("display notification %s with title \"Flashlight\""%(asquote("Couldn't find iMessage contact for %s."%recipient)))
-      return
-    
-    set_selected_files = """
-    tell application "Finder"
-    	set selectedFiles to selection
-    end tell
-    """ if attach_selected_files else "set selectedFiles to {}"
-    
-    script = """
-    %s
-    using terms from application "Messages"
-    	tell application "Messages"
-        activate
-    		set targetService to 1st service whose service type = iMessage
-    		set targetBuddy to buddy %s of targetService
-    		send %s to targetBuddy
-    		repeat with theFile in selectedFiles
-    			send (theFile as alias) to targetBuddy
-    		end repeat
-    	end tell
-    end using terms from
-    
-    """%(set_selected_files, asquote(buddy), asquote(body))
-    print script
-    asrun(script)
+		buddy = recipient
+		
+		set_selected_files = """
+		tell application "Finder"
+			set selectedFiles to selection
+		end tell
+		""" if attach_selected_files else "set selectedFiles to {}"
+		
+		script = """
+		%s
+		using terms from application "Messages"
+			tell application "Messages"
+				activate
+				set targetService to 1st service whose service type = iMessage
+				set targetBuddy to buddy %s of targetService
+				send %s to targetBuddy
+				repeat with theFile in selectedFiles
+					send (theFile as alias) to targetBuddy
+				end repeat
+			end tell
+		end using terms from
+		
+		"""%(set_selected_files, asquote(buddy), asquote(body))
+		print script
+		asrun(script)
 
 if __name__ == '__main__':
-    send_message("rebecca plattus", "message test", True)
+		send_message("7185947958", "message test", True)
