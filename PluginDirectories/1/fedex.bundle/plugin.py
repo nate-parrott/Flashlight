956b7e48b1e67418e2f75d2e1eb966fda9431514
Simon Michel <simon.michel@gmx.net>
2015-04-25 21:45:48 +0200

    Add fedex tracking plugin

---
+++ PluginDirectories/1/fedex.bundle/plugin.py
@@ -0,0 +1,27 @@
+import urllib, json, i18n
+
+def results(parsed, original_query):
+    search_specs = [
+        ["fedex.com", "~fedexquery", "https://www.fedex.com/apps/fedextrack/?action=track&cntry_code=us&trackingnumber="]
+    ]
+    for name, key, url in search_specs:
+        if key in parsed:
+            url = i18n.localstr(url)
+            search_url = url + urllib.quote_plus(parsed[key].encode('utf-8'))
+            return {
+                "title": i18n.localstr("Search for '{0}'").format(parsed[key].encode('utf-8')),
+                "run_args": [search_url],
+                "html": """
+                <script>
+                setTimeout(function() {
+                    window.location = %s
+                }, 500);
+                </script>
+                """%(json.dumps(search_url)),
+                "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
+                "webview_links_open_in_browser": True
+            }
+
+def run(url):
+    import os
+    os.system('open "{0}"'.format(url))
